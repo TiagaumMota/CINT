94e9c7076292b42f0964c67c54f7d236096e0f1a
FilipaLino <102179022+FilipaLino@users.noreply.github.com>
2022-10-12 11:44:10 +0100

    Corrigi para dar split no val set

--- Proj1.py
+++ Proj1.py
@@ -30,10 +30,10 @@ def remove_outliers(df):
         df_new=df
 
         for index in range(2,11):
-            column=df.iloc[:,index].to_numpy()
+            column = df.iloc[:,index].to_numpy()
             z = np.abs(stats.zscore(column))
             
-            outliers=np.where(z > threshold)
+            outliers = np.where(z > threshold)
             df_new.drop(outliers[0], inplace=True)
 
         return df_new
@@ -44,20 +44,23 @@ def remove_outliers(df):
 df = pd.read_csv('Proj1_Dataset.csv',decimal='.', sep=',' )
 
 #Include the binary classification output 
-df['MoreThan2']=(df['Persons']>2).astype(int)
+df['MoreThan2'] = (df['Persons']>2).astype(int)
 
 #Data Preprocessing
 #Filling all the missing values
-clean_df=df.interpolate(option='time')
+clean_df = df.interpolate(option='time')
 
 #Find some outliers and remove them 
-clean_df=remove_outliers(clean_df)
-print(clean_df.shape)
+clean_df = remove_outliers(clean_df)
 
-#Resolve inconsistencies?
-inconsistencies=np.where((clean_df['Persons']==0 ) & (clean_df['PIR1']+clean_df['PIR2']!=0))
-#print(inconsistencies[0].shape)
-#print(clean_df.iloc[inconsistencies[0], :])
+#Resolve inconsistencies - correct them or delete?
+inconsistencies = np.where( (clean_df['Persons'] == 0 ) & (clean_df['PIR1']+clean_df['PIR2'] != 0) )
+#clean_df.iloc[inconsistencies[0], 9:11] = 0
+#clean_df.drop(inconsistencies[0], inplace=True)
+
+train_ratio = 0.70
+validation_ratio = 0.15
+test_ratio = 0.15
 
 #Baseline MLP
 print("Problem a)")
@@ -65,30 +68,32 @@ print("Problem a)")
 X=  clean_df.iloc[:, 2:11].to_numpy()
 Y=np.reshape(clean_df['MoreThan2'].to_numpy(), (-1,1))
 
-x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2 , shuffle = False)
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=1 - train_ratio , shuffle = False)
+x_val, x_test, y_val, y_test = train_test_split(x_test, y_test, test_size=test_ratio/(test_ratio + validation_ratio), shuffle=False)
 
 clf = MLPClassifier(hidden_layer_sizes=(10,),activation='logistic',solver='sgd',random_state=1, max_iter=300)
 clf.fit(x_train, y_train)
-y_pred=clf.predict(x_test).reshape(-1,1)
+y_pred=clf.predict(x_val).reshape(-1,1)
 
-print("Accuracy:", accuracy_score(y_test, y_pred ))
-print("Pecision:", precision_score(y_test, y_pred))
-print("Recall:", recall_score(y_test, y_pred))
-print("F1-Score:", f1_score(y_test, y_pred))
-print("Confusion Matrix:", confusion_matrix(y_test, y_pred))
+print("Accuracy:", accuracy_score(y_val, y_pred ))
+print("Pecision:", precision_score(y_val, y_pred))
+print("Recall:", recall_score(y_val, y_pred))
+print("F1-Score:", f1_score(y_val, y_pred))
+print("Confusion Matrix:", confusion_matrix(y_val, y_pred))
 
-print("Problem b)")
+print("\n Problem b)")
 Y=np.reshape(clean_df['Persons'].to_numpy(), (-1,1))
 
-x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=0.2 , shuffle = False)
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size=1 - train_ratio , shuffle = False)
+x_val, x_test, y_val, y_test = train_test_split(x_test, y_test, test_size=test_ratio/(test_ratio + validation_ratio), shuffle=False)
 
 clf = MLPClassifier(hidden_layer_sizes=(10,),activation='logistic',solver='sgd',random_state=1, max_iter=300)
 clf.fit(x_train, y_train)
-y_pred=clf.predict(x_test).reshape(-1,1)
+y_pred=clf.predict(x_val).reshape(-1,1)
 
-print("Accuracy:", accuracy_score(y_test, y_pred))
-print("Pecision:", precision_score(y_test, y_pred, average='macro'))
-print("Recall:", recall_score(y_test, y_pred, average='macro'))
-print("F1-Score:", f1_score(y_test, y_pred, average='macro'))
-print("Confusion Matrix:", confusion_matrix(y_test, y_pred))
+print("Accuracy:", accuracy_score(y_val, y_pred))
+print("Pecision:", precision_score(y_val, y_pred, average='macro'))
+print("Recall:", recall_score(y_val, y_pred, average='macro'))
+print("F1-Score:", f1_score(y_val, y_pred, average='macro'))
+print("Confusion Matrix:", confusion_matrix(y_val, y_pred))
 
